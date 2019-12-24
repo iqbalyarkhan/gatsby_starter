72dda958b33ac5534d82b28d449a55233f2848e3
iqbalyarkhan <iqbal.khan@utdallas.edu>
2019-12-24 16:43:00 -0600

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="PageName" keywords={[`gatsby`, `application`, `react`]} />
+    <h3>This page is for what I'm about</h3>
+  </Layout>
+)
+
+export default AboutPage
